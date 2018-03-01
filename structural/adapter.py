42852250a7fa9ac654ecb7612d24ed4e9033850d
Taras Nytiaha <taras.nytiaha@gmail.com>
2018-03-01 11:28:58 +0200

    Add pattern example

--- structural/adapter.py
+++ structural/adapter.py
@@ -1,3 +1,4 @@
+"""This Adapter pattern example"""
 
 
 class Computer:
@@ -8,7 +9,7 @@ class Computer:
         return self.name
 
     def switch_settings(self):
-        return f'switch_settings'
+        return f'switch settings'
 
 
 class Tv:
@@ -19,7 +20,7 @@ class Tv:
         return self.name
 
     def switch_channel(self):
-        return f'{self.name} switch channel'
+        return f'switch channel'
 
 
 class Radio:
@@ -30,7 +31,7 @@ class Radio:
         return self.name
 
     def switch_station(self):
-        return f'{self.name} switch station'
+        return f'switch station'
 
 
 class Microwave:
@@ -42,7 +43,7 @@ class Microwave:
 
     def power_mode(self):
 
-        return f'{self.name} power mode'
+        return f'power mode'
 
 
 class TechDevices:
@@ -50,11 +51,38 @@ class TechDevices:
 
     def __init__(self, object, method):
         self.object = object
-        self.__dict__.uptade(dict(method))
+        self.__dict__.update(method)
 
     def __str__(self):
-        return self.object
+        return str(self.object)
 
 
 if __name__ == '__main__':
-    pass
+    list_objects = []
+    object_computer = Computer('Notebook')
+    object_tv = Tv('Home Tv')
+    object_radio = Radio('Radio receivers')
+    object_microwave = Microwave('Stove')
+
+    list_objects.append(
+        TechDevices(
+            object_computer, dict(
+                switch=object_computer.switch_settings)))
+
+    list_objects.append(
+        TechDevices(
+            object_tv, dict(
+                switch=object_tv.switch_channel)))
+
+    list_objects.append(
+        TechDevices(
+            object_radio, dict(
+                switch=object_radio.switch_station)))
+    
+    list_objects.append(
+        TechDevices(
+            object_microwave, dict(
+                switch=object_microwave.power_mode)))
+
+    for item in list_objects:
+        print(f'{item} {item.switch()}')
